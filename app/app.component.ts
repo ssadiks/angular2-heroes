0134ac1c6c7e7fb04a0b4a79e663605f5fb02d82
unknown <sadiks.pro@gmail.com>
2016-03-14 21:37:06 +0100

    Step 1 : The Hero Editor

---
+++ app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component} from 'angular2/core';
+
+interface Hero {
+    id: number;
+    name: string;
+}
+
+@Component({
+    selector: 'my-app',
+    template:'<h1>{{title}}</h1>' +
+    '<h2>{{hero.name}} details!</h2>' +
+    '<div><label>id: </label>{{hero.id}}</div>' +
+    '<div><label>name: </label></div>'+
+    '<div><input [(ngModel)]="hero.name" placeholder="name"></div>'+
+    ''})
+
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Lionel Messi'
+    };
+}
+
